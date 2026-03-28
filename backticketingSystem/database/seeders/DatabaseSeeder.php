1c3d8547c00cf8e8c1205773a52268ded0d3bb8a
meljz <reyesmeljohnjay@gmail.com>
2026-03-28 20:48:09 +0800

    added seeder

--- backticketingSystem/database/seeders/DatabaseSeeder.php
+++ backticketingSystem/database/seeders/DatabaseSeeder.php
@@ -4,12 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Database\Seeders\MockuserSeed;
 
 class DatabaseSeeder extends Seeder
 {
     
     public function run()
     {
-        $this->call('MockuserSeed'::class);
+        $this->call(MockuserSeed::class);
     }
 }
