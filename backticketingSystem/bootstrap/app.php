a93898fc26b2eb3390ec6b53ab1ed2d0a17503f0
meljz <reyesmeljohnjay@gmail.com>
2026-06-13 11:53:59 +0800

    added dockerfile

--- backticketingSystem/bootstrap/app.php
+++ backticketingSystem/bootstrap/app.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+/*
+|--------------------------------------------------------------------------
+| Load Environment Variables
+|--------------------------------------------------------------------------
+*/
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
 ))->bootstrap();
@@ -12,49 +17,53 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
 */
-
 $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
 $app->withFacades();
 $app->withEloquent();
-//$app->loadEnvironmentFrom('.env');
 
+/*
+|--------------------------------------------------------------------------
+| Register Configuration
+|--------------------------------------------------------------------------
+*/
+$app->configure('app');
 
-$app->register(App\Providers\AuthServiceProvider::class);
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+*/
 
-$app->routeMiddleware([
-    'auth' => App\Http\Middleware\Authenticate::class,
-    Fruitcake\Cors\HandleCors::class,
-]);
-
+// Global middleware (CORS must be FIRST)
 $app->middleware([
     App\Http\Middleware\CorsMiddleware::class,
 ]);
 
-
-
+/*
+|--------------------------------------------------------------------------
+| Route Middleware
+|--------------------------------------------------------------------------
+*/
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
-| Register Container Bindings
+| Service Providers
 |--------------------------------------------------------------------------
-
-
-|
-| Now we will register a few bindings in the service container. We will
-| register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
-|
 */
+$app->register(App\Providers\AuthServiceProvider::class);
 
+/*
+|--------------------------------------------------------------------------
+| Exception Handler
+|--------------------------------------------------------------------------
+*/
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
@@ -67,62 +76,9 @@ $app->singleton(
 
 /*
 |--------------------------------------------------------------------------
-| Register Config Files
+| Load Routes
 |--------------------------------------------------------------------------
-|
-| Now we will register the "app" configuration file. If the file exists in
-| your configuration directory it will be loaded; otherwise, we'll load
-| the default version. You may register other files below as needed.
-|
 */
-
-$app->configure('app');
-
-/*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
-
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
-
-/*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
-
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
-
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
